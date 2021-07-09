01f098f1bee2aeb9f8cf0b95679e930274694f6f
Dmitry Oshurkov <dm.oshurkov@prof-itgroup.ru>
2021-07-09 18:35:21 +0300

    + popover main_menu

--- src/nativeMain/kotlin/name/oshurkov/viget/main.kt
+++ src/nativeMain/kotlin/name/oshurkov/viget/main.kt
@@ -3,10 +3,10 @@ package name.oshurkov.viget
 import binding.VigetUI
 import org.gnome.gtk.ApplicationFactory
 import org.gnome.gtk.addWindow
+import org.gnome.gtk.asButton
 import org.gnome.gtk.asDialog
 import org.gnome.gtk.asWidget
 import org.gnome.gtk.hide
-import org.gnome.gtk.onActivate
 import org.gnome.gtk.onClicked
 import org.gnome.gtk.run
 import org.gnome.gtk.showAll
@@ -19,7 +19,7 @@ fun main() = ApplicationFactory.newAndRun("name.oshurkov.viget") {
         newButton.onClicked {
         }
 
-        aboutMenuItem.onActivate {
+        aboutMenuItem.asButton.onClicked {
             aboutDialog.asDialog.run()
             aboutDialog.asWidget.hide()
         }
